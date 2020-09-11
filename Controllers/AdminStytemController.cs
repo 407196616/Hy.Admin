2365076812ce80874e379bb08a3147fe9bdf2a4d
JHongChen <407196616@qq.com>
2020-09-11 16:05:42 +0800

    0.0.1

---
+++ Controllers/AdminStytemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hy.Admin.Controllers
+{
+    public class AdminStytem : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult View23()
+        {
+            return View();
+        }
+    }
+}
